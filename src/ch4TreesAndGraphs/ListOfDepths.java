96c32339b03a6f9a168efe8eb2fd2ff3df18ea39
subrat <subrat.kumar025@gmail.com>
2022-02-19 16:36:57 +0530

    List of list at particular depth

---
+++ src/ch4TreesAndGraphs/ListOfDepths.java
@@ -0,0 +1,67 @@
+package ch4TreesAndGraphs;
+
+import com.google.common.base.Preconditions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ListOfDepths<E> {
+    private final Node<E> root;
+    private int size;
+    private static class Node<E> {
+        E data;
+        Node<E> left;
+        Node<E> right;
+        Node(E data) {
+            this.data = data;
+        }
+    }
+
+    public ListOfDepths(E[] array) {
+        Preconditions.checkArgument(array.length > 0);
+        root = createBinaryTree(array, 0 , array.length);
+    }
+
+    private Node<E> createBinaryTree(E[] sortedArray, int start, int end) {
+        if (start >= end) {
+            return null;
+        }
+        int mid = start + (end - start)/2;
+        Node<E> node = new Node<>(sortedArray[mid]);
+        node.left = createBinaryTree(sortedArray, start, mid);
+        node.right = createBinaryTree(sortedArray, mid+ 1, end);
+        return node;
+    }
+
+    public List<List<E>> listOfDepths() {
+        List<List<E>> listOfDepths = new ArrayList<>();
+        List<E> levelList;
+        Queue<Node<E>> queue = new LinkedList<>();
+        int currRowCount = 0;
+        int nextRowCount = 0;
+
+        queue.add(root);
+        currRowCount++;
+        while (!queue.isEmpty()) {
+            levelList = new LinkedList<>();
+            while (currRowCount-- > 0 ) {
+                Node<E> node = queue.remove();
+                levelList.add(node.data);
+                if (node.left != null) {
+                    queue.add(node.left);
+                    nextRowCount++;
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                    nextRowCount++;
+                }
+            }
+            currRowCount = nextRowCount;
+            nextRowCount = 0;
+            listOfDepths.add(levelList);
+        }
+        return listOfDepths;
+    }
+}
